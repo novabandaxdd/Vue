07a54ab3eebd76d3fbe43e2ae824301cbf6f7a59
novabandaxdd <48572002+novabandaxdd@users.noreply.github.com>
2019-05-06 17:15:29 -0300

    Add files via upload

---
+++ Vue/backup/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+    <AddTodo v-on:add-todo="addTodo"/>
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Tarefa 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Tarefa 2",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Tarefa 3",
+          completed: false
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos.push(newTodo);
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
